2f4a95e52acd813b3c661cd61e8c722ee2f29271
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-03-14 13:58:07 +0900

    Update PKH.py

--- 공통문제/3주차/소수 찾기/PKH.py
+++ 공통문제/3주차/소수 찾기/PKH.py
@@ -1,6 +1,5 @@
 from itertools import permutations
 def solution(numbers):
-    answer = 0
     num = list(numbers)
     for i in range(2,len(numbers)+1):
         n = list(permutations(numbers,i))
@@ -9,18 +8,22 @@ def solution(numbers):
                 num.append(''.join(j))
     num = list(set([int(x) for x in num]))
     
+    # 0과 1은 제외
     if num.count(1):
         num.remove(1)
     if num.count(0):
         num.remove(0)
+    
     num.sort()
-    print(num)
+    answer = len(num)
     for i in num:
         x = 2
         while x**2 <= i:
+            # 소수가 아니면 뺀다.
             if i % x == 0:
                 answer -= 1
                 break
             x += 1
-        answer += 1
     return answer
+
+# permutations는 순열을 구하는 모듈이다.
