77011e5f5f74eaae2d18f24053371ff89f2a01e0
KSJ <37233732+Gapus@users.noreply.github.com>
2022-03-19 22:47:05 +0900

    약수의 개수와 덧셈 - KSJ

--- 개인문제/김성중/약수의개수와덧셈.js
+++ 개인문제/김성중/약수의개수와덧셈.js
@@ -29,3 +29,23 @@ function solution(left, right) {
 }
 
 solution(13, 17);
+
+// 해답
+// function solution(left, right) {
+//   var count = 0;
+//   var answer = 0;
+//   for(var i=left; i<=right; i++){
+//        for(var j=1; j<=i; j++){
+//            if(i%j === 0 ){
+//                count += 1;
+//            }
+//       }
+//       if(count % 2 === 0){
+//           answer += i;
+//       }else{
+//           answer -= i;
+//       }
+//       count =0;
+//   }
+//   return answer;
+// }
