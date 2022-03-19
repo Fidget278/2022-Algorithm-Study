4e895f369644d2d6d459af13e09fff0bc5df8a8c
KSJ <37233732+Gapus@users.noreply.github.com>
2022-03-19 22:43:36 +0900

    H-index -KSJ

--- 공통문제/5주차/H-Index/KSJ.js
+++ 공통문제/5주차/H-Index/KSJ.js
@@ -12,6 +12,9 @@ function solution(citations) {
     if (i < citations[i]) {
       answers++;
     }
+    // console.log(
+    //     "i : " + i + ", answers : " + answers + ", citations[i] : " + citations[i]
+    //   );
   }
 
   return answers;
@@ -25,25 +28,46 @@ console.log(3);
 solution([6, 6, 6, 6, 6, 6]);
 console.log(4);
 solution([2, 2, 2]);
+console.log(5);
+solution([47, 42, 32, 28, 24, 22, 17, 15, 10, 10, 8]);
+console.log(6);
+solution([3, 0, 6, 1, 5]);
 
 // 1
-// h : 1, length : 4
-// h : 2, length : 3
-// h : 3, length : 3
-// h : 4, length : 2
+// i : 0, answers : 1, citations[i] : 6
+// i : 1, answers : 2, citations[i] : 5
+// i : 2, answers : 3, citations[i] : 3
+// i : 3, answers : 3, citations[i] : 1
+// i : 4, answers : 3, citations[i] : 0
 // 2
-// h : 1, length : 2
-// h : 2, length : 2
-// h : 3, length : 2
+// i : 0, answers : 1, citations[i] : 100
+// i : 1, answers : 2, citations[i] : 10
 // 3
-// h : 1, length : 6
-// h : 2, length : 6
-// h : 3, length : 6
-// h : 4, length : 6
-// h : 5, length : 6
-// h : 6, length : 6
-// h : 7, length : 0
+// i : 0, answers : 1, citations[i] : 6
+// i : 1, answers : 2, citations[i] : 6
+// i : 2, answers : 3, citations[i] : 6
+// i : 3, answers : 4, citations[i] : 6
+// i : 4, answers : 5, citations[i] : 6
+// i : 5, answers : 6, citations[i] : 6
 // 4
-// h : 1, length : 3
-// h : 2, length : 3
-// h : 3, length : 0
+// i : 0, answers : 1, citations[i] : 2
+// i : 1, answers : 2, citations[i] : 2
+// i : 2, answers : 2, citations[i] : 2
+// 5
+// i : 0, answers : 1, citations[i] : 47
+// i : 1, answers : 2, citations[i] : 42
+// i : 2, answers : 3, citations[i] : 32
+// i : 3, answers : 4, citations[i] : 28
+// i : 4, answers : 5, citations[i] : 24
+// i : 5, answers : 6, citations[i] : 22
+// i : 6, answers : 7, citations[i] : 17
+// i : 7, answers : 8, citations[i] : 15
+// i : 8, answers : 9, citations[i] : 10
+// i : 9, answers : 10, citations[i] : 10
+// i : 10, answers : 10, citations[i] : 8
+// 6
+// i : 0, answers : 1, citations[i] : 6
+// i : 1, answers : 2, citations[i] : 5
+// i : 2, answers : 3, citations[i] : 3
+// i : 3, answers : 3, citations[i] : 1
+// i : 4, answers : 3, citations[i] : 0
