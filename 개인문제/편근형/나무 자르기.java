25fab5e93e1cfc698713a804a7150c72d8436778
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-03-26 04:23:44 +0900

    Create 나무 자르기.java

---
+++ 개인문제/편근형/나무 자르기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] heights = new int[n];
+    int answer = 0;
+    st = new StringTokenizer(br.readLine(), " ");
+    int start, mid, end;
+    start=0; end=0;
+    for(int i = 0; i < n; i++) {
+      heights[i] = Integer.parseInt(st.nextToken());
+      if(heights[i] > end) {
+        end = heights[i];
+      }
+    }
+    
+    while(start <= end) {
+      mid = (start+end) / 2;
+      long temp = 0;
+      for(int i : heights) {
+        temp += i >= mid ? i-mid : 0;
+      }
+      if (temp < m) {
+        end = mid - 1;
+      }
+      else {
+        answer = mid;
+        start = mid + 1;
+      }
+    }
+    System.out.print(answer);
+  }
+}
