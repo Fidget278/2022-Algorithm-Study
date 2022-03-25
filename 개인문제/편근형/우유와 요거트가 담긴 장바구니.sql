be8273ff8c1ed49badca0d62db4b4a079770d505
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-03-25 18:01:08 +0900

    Update and rename 우유와 요거트가 담긴 장바구니 to 우유와 요거트가 담긴 장바구니.sql

--- 개인문제/편근형/우유와 요거트가 담긴 장바구니.sql
+++ 개인문제/편근형/우유와 요거트가 담긴 장바구니.sql
@@ -1,4 +1,4 @@
-https://programmers.co.kr/learn/courses/30/lessons/62284
+-- https://programmers.co.kr/learn/courses/30/lessons/62284
 
 SELECT milk.cart_id
 FROM (SELECT DISTINCT cart_id FROM cart_products WHERE name='Milk') AS milk
