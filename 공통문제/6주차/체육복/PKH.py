82c674bcc6613acd148f212ae52cfe884b62e0e7
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-04-04 15:32:02 +0900

    Update PKH.py

--- 공통문제/6주차/체육복/PKH.py
+++ 공통문제/6주차/체육복/PKH.py
@@ -1,6 +1,6 @@
 def solution(n, lost, reserve):
-    set_reserve = set(reserve) - set(lost)
-    set_lost = set(lost) - set(reserve)
+    set_reserve = set(reserve) - set(lost) # 여별옷을 가지고 있는 사람 중 원래 체육복을 잃어버린 사람을 제거.
+    set_lost = set(lost) - set(reserve) # 잃어버린 사람 중 여벌옷을 가지고 있는 사람을 제거
     
     for i in set_reserve:
         if i-1 in set_lost:
