03d457501cbe6ee9d4d13b4c592c0143867d8ddd
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-05-03 18:31:04 +0900

    Update PKH.py

--- 공통문제/8주차/크레인 인형뽑기 게임/PKH.py
+++ 공통문제/8주차/크레인 인형뽑기 게임/PKH.py
@@ -25,3 +25,8 @@ def solution(board, moves):
             lst.pop(); lst.pop()
             
     return cnt
+
+
+# 자료구조 중 덱을 이용. 스택이지만 이게 편해서 했다.
+# board를 층별로 아닌 열별로 재정렬을 한다. moves가 열로 넘버링이 되어있기 때문이다.
+# 스택에서 두개가 연속이 되면 pop 할 것.
