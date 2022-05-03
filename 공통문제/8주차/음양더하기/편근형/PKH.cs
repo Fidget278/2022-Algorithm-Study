dea00773d9caf8e5709d1d2e2fdad587ecc70666
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-05-03 21:52:58 +0900

    Create PKH.cs

---
+++ 공통문제/8주차/음양더하기/편근형/PKH.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] absolutes, bool[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.Length; i++) {
+            if (signs[i] == true) {
+                answer += absolutes[i];
+            }
+            else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
