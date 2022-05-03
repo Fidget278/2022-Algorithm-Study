e68fd2c852657bb2f063f704ef42f51e28ff1d83
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-05-03 21:54:38 +0900

    Update PKH.java

--- 공통문제/8주차/음양더하기/편근형/PKH.java
+++ 공통문제/8주차/음양더하기/편근형/PKH.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        int len = absolutes.length;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < absolutes.length; i++) {
             if (signs[i] == true) {
                 answer += absolutes[i];
             }
