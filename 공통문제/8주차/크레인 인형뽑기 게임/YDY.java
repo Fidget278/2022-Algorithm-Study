2b9bf82f0fbd6e81368cdf7cacbbcd129b44eb3f
da77777 <84134297+da77777@users.noreply.github.com>
2022-05-07 01:39:55 +0900

    Update and rename AlgorithmStudy9.java to YDY.java

--- 공통문제/8주차/크레인 인형뽑기 게임/YDY.java
+++ 공통문제/8주차/크레인 인형뽑기 게임/YDY.java
@@ -60,11 +60,4 @@ public class AlgorithmStudy9 {
             return answer;
         }
     }
-
-    public static void main(String[] args) {
-        int[][] b = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] m = {1,5,3,5,1,2,1,4};
-        Solution1.solution(b, m);
-        Solution2.solution(b, m);
-    }
 }
