67623a064f00de83b010aac01f36de8582d8472f
Gapus <37233732+Gapus@users.noreply.github.com>
2022-05-17 16:09:39 +0900

    Update KSJ.js

--- 공통문제/9주차/버블 소트/KSJ.js
+++ 공통문제/9주차/버블 소트/KSJ.js
@@ -25,41 +25,41 @@
 //   }
 // }
 
-// const readline = require("readline");
+const readline = require("readline");
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-// const input = [];
-// let count = 0;
-// rl.on("line", (line) => {
-//     if (!count) {
-//         count = Number(line);
-//     } else {
-//         input.push(Number(line));
-//         if (input.length === count) {
-//             rl.close();
-//         }
-//     }
-// }).on("close", () => {
-//     const numbers = input
-//         .map((el, i) => {
-//             return {
-//                 value: el,
-//                 index: i,
-//             };
-//         })
-//         .sort((a, b) => a.value - b.value);
+const input = [];
+let count = 0;
+rl.on("line", (line) => {
+    if (!count) {
+        count = Number(line);
+    } else {
+        input.push(Number(line));
+        if (input.length === count) {
+            rl.close();
+        }
+    }
+}).on("close", () => {
+    const numbers = input
+        .map((el, i) => {
+            return {
+                value: el,
+                index: i,
+            };
+        })
+        .sort((a, b) => a.value - b.value);
 
-//     let maxValue = 0;
-//     for (let i = 0; i < count; i++) {
-//         maxValue = Math.max(maxValue, numbers[i].index - i);
-//     }
+    let maxValue = 0;
+    for (let i = 0; i < count; i++) {
+        maxValue = Math.max(maxValue, numbers[i].index - i);
+    }
 
-//     console.log(maxValue + 1);
-// });
+    console.log(maxValue + 1);
+});
 
 // function sol(input) {
 //     const N = +input[0];
