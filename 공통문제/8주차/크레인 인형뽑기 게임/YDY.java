7604bb1359acda58cf907d883fdecd060eba1039
da77777 <84134297+da77777@users.noreply.github.com>
2022-05-17 18:07:45 +0900

    Update YDY.java

--- 공통문제/8주차/크레인 인형뽑기 게임/YDY.java
+++ 공통문제/8주차/크레인 인형뽑기 게임/YDY.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,20 +12,22 @@ public class AlgorithmStudy9 {
             int answer = 0;
             List<Integer> list = new ArrayList<>();
 
+            System.out.println(Arrays.toString(moves));
+
             for(int i = 0; i < moves.length; i++) {
-                int m = moves[i] - 1;
-                
+                int m = moves[i] - 1; //들어갈 열 값을 index로 설정해줘야 하므로 -1
+
                 for(int j = 0; j < board.length; j++) {
                     int b = board[j][m];
-                    
+
                     if (b != 0) {
                         list.add(b);
-                        board[j][m] = 0;
-                        
-                        if (list.size() > 1 && list.get(list.size() - 1) == list.get(list.size() - 2)) {
-                            list.remove(list.size()-1);
-                            list.remove(list.size()-1);
-                            answer += 2;
+                        board[j][m] = 0; //인형 뽑았으니까 0으로 바꿔
+
+                        if (list.size() > 1 && list.get(list.size() - 1) == list.get(list.size() - 2)) { //바구니에 2개 이상 인형이 있고, 마지막 인형과 그 앞의 인형이 동일하면
+                            list.remove(list.size()-1); //list에서 마지막 인형 빼기
+                            list.remove(list.size()-1); //list에서 마지막 인형 빼기
+                            answer += 2; //2개 뺐으니까 +=2
                         }
                         break;
                     }
@@ -45,16 +48,16 @@ public class AlgorithmStudy9 {
 
                 for(int j = 0; j < board.length; j++) {
                     int b = board[j][m];
-                    
+
                     if (b != 0) {
                         board[j][m] = 0;
 
                         if(stack.isEmpty() || b != stack.peek()) {
                             stack.push(b);
                             break;
-                        } else if(b == stack.peek()) {
+                        } else if(b == stack.peek()) { //넣거나 빼지 않고 b와 마지막 값 바로 비교
                             answer += 2;
-                            stack.pop();
+                            stack.pop(); //마지막 값 빼내기
                             break;
                         }
                     }
@@ -65,4 +68,10 @@ public class AlgorithmStudy9 {
         }
     }
 
+    public static void main(String[] args) {
+        int[][] b = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] m = {1,5,3,5,1,2,1,4};
+        Solution1.solution(b, m);
+        //Solution2.solution(b, m);
+    }
 }
