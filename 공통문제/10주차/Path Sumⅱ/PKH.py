e7ffdf0d5dbdd5840760e68b300a9837e15d7b93
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-05-18 22:27:52 +0900

    Create PKH.py

---
+++ 공통문제/10주차/Path Sumⅱ/PKH.py
@@ -0,0 +1,37 @@
+# 시간 : 52ms
+from copy import deepcopy
+
+class Solution:
+    def pathSum(self, root, targetSum):
+        answer = []
+        def check(lst, target, cur):
+            if cur.left == None and cur.right == None:
+                ret = sum(lst)
+                if ret == target:
+                    ans = deepcopy(lst)
+                    answer.append(ans)
+                lst.pop()
+                return 
+            left = True if cur.left != None else False
+            right = True if cur.right != None else False
+            if left:
+                new_cur = cur.left
+                lst.append(new_cur.val)
+                check(lst, target, new_cur)
+                left = False
+            if right:
+                new_cur = cur.right
+                lst.append(new_cur.val)
+                check(lst, target, new_cur)
+                right = False
+            if left == False and right == False:
+                lst.pop()
+                
+                
+        now = root
+        q = []
+        if now == None:
+            return []
+        q.append(now.val)
+        check(q, targetSum, now)
+        return answer
