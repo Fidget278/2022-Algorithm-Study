7305f92ca2e1ecf58c812e64d2077c56b2b28e22
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-06-04 01:48:29 +0900

    Create PKH.py

---
+++ 공통문제/12주차/All Paths From Source to Target/PKH.py
@@ -0,0 +1,27 @@
+from collections import defaultdict,deque
+from copy import deepcopy
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        answer = []
+        d = defaultdict(deque)
+        for i in range(len(graph)):
+            for j in graph[i]:
+                d[i].append(j)
+        if len(d[0]) == 0:
+            return []
+        
+        n = len(graph)
+        def check(lst, target):
+            if target == n-1:
+                ret = deepcopy(lst)
+                answer.append(ret)
+                lst.pop()
+                return
+            for a in d[target]:
+                lst.append(a)
+                check(lst,a)
+            lst.pop()
+        
+        q = [0]
+        check(q, 0)
+        return answer
