2676e7010ae083a283177d62c38fd91e6b770519
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-08-26 01:38:32 +0900

    Update 행렬과 연산.py

--- 개인문제/편근형/행렬과 연산.py
+++ 개인문제/편근형/행렬과 연산.py
@@ -18,3 +18,31 @@ def solution(rc, operations):
                 b = c
                 temp[j].appendleft(temp[j+1].popleft())
     return [list(i) for i in temp]
+
+
+# 전부 만점
+from collections import deque
+def solution(rc, operations):
+    l = len(rc)
+    row = deque(deque(i[1:-1]) for i in rc)
+    col = [deque(rc[i][0] for i in range(l)), deque([rc[i][len(rc[0])-1] for i in range(l)])]
+    for i in operations:
+        if i[0] == "S":
+            if len(row) != 0:
+                row.appendleft(row.pop())
+            col[0].appendleft(col[0].pop())
+            col[1].appendleft(col[1].pop())
+        else:
+            if len(row) != 0:
+                row[l-1].append(col[1].pop())
+            col[0].append(row[l-1].popleft())
+            row[0].appendleft(col[0].popleft())
+            col[1].appendleft(row[0].pop())
+    
+    answer = []
+    for i in range(l):
+        answer.append([])
+        answer[i].append(col[0][i])
+        answer[i].extend(row[i])
+        answer[i].append(col[1][i])
+    return answer
