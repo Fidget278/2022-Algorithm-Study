b952f32f61a48aac8ede51cea4c5b27bc452115e
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-11-27 22:29:06 +0900

    Update climbingStairs.py

--- 개인문제/편근형/climbingStairs.py
+++ 개인문제/편근형/climbingStairs.py
@@ -2,10 +2,8 @@
 
 def solution(n):
     dp = [0 for i in range(n+1)]
-    if n == 1: return 1
-    if n == 2: return 2
-    dp[1] = 1
-    dp[2] = 2
+    if n <=2:
+        return n
     for i in range(3, n+1):
         dp[i] = dp[i-1] + dp[i-2]
     return dp[n]
