07b52b90b2f460592388b14ec9289039b88a431d
Geunhyung Pyun <58629366+jollypyun@users.noreply.github.com>
2022-11-27 21:38:11 +0900

    Create climbingStairs.py

---
+++ 개인문제/편근형/climbingStairs.py
@@ -0,0 +1,11 @@
+# 링크 : https://app.codesignal.com/interview-practice/question/oJXTWuwEZiC6FTw3A/solutions
+
+def solution(n):
+    dp = [0 for i in range(n+1)]
+    if n == 1: return 1
+    if n == 2: return 2
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
